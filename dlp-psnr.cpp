0634fff5df7deb7022b3f0398e0cefc804f1e7ce
jendralhxr <jendral.hxr@gmail.com>
2016-06-08 11:22:45 +0900

    little bit cleanup

--- dlp-psnr.cpp
+++ dlp-psnr.cpp
@@ -12,7 +12,7 @@ int main( int argc, char** argv )
 {
 
 Mat image1, image2;
-Mat imagedark, imagebright, imageinterpolate;
+Mat imagedark, imagebright;
 
 bool background;
 double mse;
@@ -21,23 +21,19 @@ double mse;
 	
 	imagedark = Mat(image1.rows, image1.cols, CV_8UC1);
 	imagebright = Mat(image2.rows, image2.cols, CV_8UC1);
-	imageinterpolate = Mat(image2.rows, image2.cols, CV_8UC1);
 	
 	cvtColor(image1, imagedark, CV_BGR2GRAY);
 	cvtColor(image2, imagebright, CV_BGR2GRAY);
 	
 	for (int j=0; j<imagedark.rows; j++){
 		for (int i=0; i<imagedark.cols; i++){
-			//imageinterpolate.data[j*imagedark.cols+i] = \
-			 sqrt(imagedark.data[j*imagedark.cols+i]+imagebright.data[j*imagedark.cols+i])*imagedark.data[j*imagedark.cols+i];
 			mse += pow(imagedark.data[j*imagedark.cols+i]-imagebright.data[j*imagedark.cols+i],2);
 			}
 		}
 	
-	printf("mse: %f\n",sqrt(mse/imagedark.cols*imagedark.rows));
-	//imwrite(argv[3], imageinterpolate);
-	imshow("interpolate", imageinterpolate);
-
+	if (mse==0) printf("0.0\n");
+	printf("mse: %f\n",mse/imagedark.cols/imagedark.rows);
+	
 // imshow( "bright", imagebright );
 // imshow( "dark", imagedark);
  
