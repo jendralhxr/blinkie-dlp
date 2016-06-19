b94484d31015810b847f790d66728cd9be37efc9
jendralhxr <jendral.hxr@gmail.com>
2016-06-19 18:37:20 +0900

    blue channel manipulated for on/off calibration

--- dlp-webcam.cpp
+++ dlp-webcam.cpp
@@ -9,13 +9,14 @@ using namespace cv;
 int main(int argc, char** argv)
 {
 	Mat disp;
+	Mat disp2;
 	Mat temp;
 	
     VideoCapture cap;
     // open the default camera, use something different from 0 otherwise;
     // Check VideoCapture documentation.
     Mat frame;
-    Size size(512,1024);
+    Size size(512,512);
     
     if(!cap.open(0))
         return 0;
@@ -24,15 +25,24 @@ int main(int argc, char** argv)
           cap >> frame;
           if( frame.empty() ) break; // end of video stream
           cvtColor(frame, temp, CV_BGR2GRAY);
-          
-		resize(temp, disp, size, CV_INTER_AREA);
+        
+		resize(temp, disp, size, CV_INTER_AREA); // resize
 	
-		for (int offset=disp.rows*disp.cols; offset>-1; offset--){
+	for (int offset=disp.rows*disp.cols; offset>-1; offset--){
 			disp.data[offset]= binaryToGray(disp.data[offset]);
 			}
-		// may need to scale/resize
-		
-          imshow("this is you, smile! :)", disp);
+			
+		  cvtColor(disp, disp2, CV_GRAY2BGR);  // b
+    
+    // all bright and all dark
+    
+    
+	for (int offset=disp2.rows*disp2.cols*3; offset>-1; offset-=3){
+			disp2.data[offset] |= 0b00000010; // blue channel
+			disp2.data[offset] &= 0b11111110;
+		}
+        
+          imshow("this is you, smile! :)", disp2);
           if( waitKey(1) == 27 ) break; // stop capturing by pressing ESC 
     }
     // the camera will be closed automatically upon exit
